583277900331b67691358e2622d449372a6b642a
Jobs Global <chris.gamella@zoho.com>
2015-03-12 18:07:28 +0800

    new sql dump

---
+++ logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2015-03-11 12:35:52 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2015-03-11T12:35:52+00:00	INFO 127.0.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
